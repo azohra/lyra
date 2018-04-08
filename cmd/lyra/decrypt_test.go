88a78f1d1e0158b547cf54f13f16014802878154
Brandon Sam Soon (LCL) <brandon.samsoon@loblaw.ca>
2018-04-08 17:38:19 -0400

    fixed more misspelling

--- cmd/lyra/decrypt_test.go
+++ cmd/lyra/decrypt_test.go
@@ -5,5 +5,5 @@ import "testing"
 func TestDecrypt(t *testing.T) {
 	file := "../../test/fixture.32.go.txt"
 	save := "../../test/fixture.322.go.txt"
-	decrypt(file, save, false, []byte("THIS IS A TEST PASSPHRASE YOU SOULD NOT USE THIS"))
+	decrypt(file, save, false, []byte("THIS IS A TEST PASSPHRASE YOU SHOULD NOT USE THIS"))
 }
--- cmd/lyra/encrypt_test.go
+++ cmd/lyra/encrypt_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestEncrypt(t *testing.T) {
 	file := "../../test/fixture.31.go.txt"
 	save := "../../test/fixture.311.go.txt"
-	encrypt(file, save, []byte("THIS IS A TEST PASSPHRASE YOU SOULD NOT USE THIS"))
+	encrypt(file, save, []byte("THIS IS A TEST PASSPHRASE YOU SHOULD NOT USE THIS"))
 
 }
